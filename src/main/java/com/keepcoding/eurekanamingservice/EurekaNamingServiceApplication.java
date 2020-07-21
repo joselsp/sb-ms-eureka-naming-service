6d0351a22fb5c07be55b1434e127496f12dca79f
Jose Luis Solis <solis.jl@gmail.com>
2020-07-21 08:39:20 -0700

    Initial commit

---
+++ src/main/java/com/keepcoding/eurekanamingservice/EurekaNamingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.keepcoding.eurekanamingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaNamingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaNamingServiceApplication.class, args);
+	}
+
+}
